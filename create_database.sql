44422a90fcd883bd02c14dd7fa302bac3b9d90d2
Nikita Mogilevsky <nikitamog@gmail.com>
2019-10-26 14:28:49 -0700

    added employee and some mild refactoring for capilization. Keyword caps and attribs are camel cased.

--- create_database.sql
+++ create_database.sql
@@ -13,15 +13,38 @@ CREATE DATABASE clean;
 USE clean;
 
 CREATE TABLE customer(
-       CustID       MEDIUMINT NOT NULL AUTO_INCREMENT, --max > 8mil.
-       fName        varchar(255) NOT NULL,
-       lName        varchar(255),
-       Street         varchar(255),
-       City            varchar(255),
-       StateInits     char(2),
-       zip            char(5), --needs review
-       email          varchar(255),
-       phone_num      varchar(20) --needs review
+       custID       MEDIUMINT NOT NULL AUTO_INCREMENT, --max > 8mil.
+       fName        VARCHAR(255) NOT NULL,
+       lName        VARCHAR(255),
+       street         VARCHAR(255),
+       city            VARCHAR(255),
+       stateInits     CHAR(2),
+       zip            CHAR(5), --needs review
+       email          VARCHAR(255),
+       phoneNum      VARCHAR(20) --needs review
        );
        
 
+/* service times can vary upon fulfillment.
+   Start and end times are stored in 'provides'. */
+CREATE TABLE service(
+       servID       MEDIUMINT NOT NULL AUTO_INCREMENT, --max > 8mil.
+       sName        VARCHAR(255) NOT NULL,
+       description  TINYTEXT, --brief description.
+       rate         DECIMAL(7, 2) UNSIGNED -- assume worst case of $50k/hr.
+       );
+
+CREATE TABLE employee(
+       eID          MEDIUMINT NOT NULL AUTO_INCREMENT, --max > 8mil.
+       gender       CHAR(1),
+       jobTitle     VARCHAR(255), --position is a keyword
+       dateHired    DATE,
+       fName        VARCHAR(255),
+       minit        CHAR(1),
+       lName        VARCHAR(255),
+       street       VARCHAR(255),
+       city         VARCHAR(100), --longest name found was 89 chars.
+       stateInits   CHAR(2),
+       zip          CHAR(5)
+       );
+       
